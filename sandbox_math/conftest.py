731df0de1a4b2553051e0eb8907cb8a28183458d
Joe <17205605+djangojoe23@users.noreply.github.com>
2023-05-31 04:30:58 -0400

    initial commit

---
+++ sandbox_math/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from sandbox_math.users.models import User
+from sandbox_math.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
